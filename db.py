40642d74826592be4d0c5ee492dd91eda9aa81f0
ijquenta <ivanjosuequentavargas@gmail.com>
2025-03-14 03:52:36 -0400

    feat/create all tables

--- db.py
+++ db.py
@@ -1,13 +1,18 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, FastAPI
 
 from sqlmodel import Session, create_engine
+from sqlmodel import SQLModel
 
 sqlite_name = "db.sqlite3"
 sqlite_url = f"sqlite:///{sqlite_name}"
 
 engine = create_engine(sqlite_url)
+
+def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
 
 def get_session():
     with Session(engine) as session:
