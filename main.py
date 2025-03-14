ed34489c32397eee71c388e7cef8e8950842a54e
ijquenta <ivanjosuequentavargas@gmail.com>
2025-03-14 02:30:49 -0400

    feat/use pydantic with a model

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+class Customer(BaseModel):
+    name: str
+    description: str | None
+    email: str
+    age: int
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/customers")
+async def create_customer(customer: Customer):
+    return customer
