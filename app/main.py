00138e0841434724a9194ed7bbce0c94d84acdde
ijquenta <ivanjosuequentavargas@gmail.com>
2025-03-17 02:19:14 -0400

    feat/agregate  transactions routes

--- app/main.py
+++ app/main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
 from models import Transaction, Invoice
 from db import create_all_tables
-from .routers import customers
+from .routers import customers, transactions
 
 app = FastAPI(lifespan=create_all_tables)
 app.include_router(customers.router)
+app.include_router(transactions.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Api Rest payments"}
 
-
-
-@app.post("/transactions")
-async def create_transaction(transaction: Transaction):
-    return transaction
 
 @app.post("/invoices")
 async def create_invoice(invoice: Invoice):
