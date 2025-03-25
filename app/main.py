f82d67249a33bbede5017e97b44a15e7ce709ea8
ijquenta <ivanjosuequentavargas@gmail.com>
2025-03-25 01:42:31 -0400

    feat/model many to many to customer to plan

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from models import Transaction, Invoice
 from db import create_all_tables
-from .routers import customers, transactions
+from .routers import customers, transactions, plans
 
 app = FastAPI(lifespan=create_all_tables)
 app.include_router(customers.router)
 app.include_router(transactions.router)
+app.include_router(plans.router)
 
 @app.get("/")
 async def root():
     return {"message": "Api Rest payments"}
-
 
 @app.post("/invoices")
 async def create_invoice(invoice: Invoice):
